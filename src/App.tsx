14e6b9344ec40da453a055b574886b5acbf581ae
Raman <raman_fartel@senla.eu>
2023-04-10 10:57:54 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {Button, message, Upload, UploadFile, UploadProps} from "antd";
+import { UploadOutlined } from '@ant-design/icons';
+import {RcFile} from "antd/es/upload";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export const App = () => {
+
+
+    const onCLick = async () => {
+        const res = await fetch('http://localhost:4000/api/dev-by')
+        const data = await res.json();
+        console.log(data)
+    }
+
+    const [fileList, setFileList] = useState<UploadFile[]>([]);
+    const [uploading, setUploading] = useState(false);
+
+    const handleUpload = () => {
+        const formData = new FormData();
+        fileList.forEach((file:  UploadFile) => {
+            formData.append('files[]', file as RcFile);
+        });
+        setUploading(true);
+        // You can use any AJAX library you like
+        fetch('http://localhost:4000/api/dev-by', {
+            method: 'POST',
+            body: formData,
+        })
+            .then((res) => res.json())
+            .then(() => {
+                setFileList([]);
+                message.success('upload successfully.');
+            })
+            .catch(() => {
+                message.error('upload failed.');
+            })
+            .finally(() => {
+                setUploading(false);
+            });
+    };
+
+    const props: UploadProps = {
+        onRemove: (file) => {
+            const index = fileList.indexOf(file);
+            const newFileList = fileList.slice();
+            newFileList.splice(index, 1);
+            setFileList(newFileList);
+        },
+        beforeUpload: (file) => {
+            setFileList([...fileList, file]);
+
+            return false;
+        },
+        fileList,
+    };
+
+
+
+    return (
+    <div>
+        <Button onClick={onCLick}>parse</Button>
+        <Upload {...props}>
+            <Button icon={<UploadOutlined />}>Select File</Button>
+        </Upload>
+        <Button
+            type="primary"
+            onClick={handleUpload}
+            disabled={fileList.length === 0}
+            loading={uploading}
+            style={{ marginTop: 16 }}
         >
-          Learn React
-        </a>
-      </header>
+            {uploading ? 'Uploading' : 'Start Upload'}
+        </Button>
     </div>
   );
 }
